b484b25df345a60a7b71af1015de26f93cd66b9c
JefersonLC <c.jeferson.lc@gmail.com>
2023-03-05 18:12:00 -0500

    create page

--- create.php
+++ create.php
@@ -1,29 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Create Task</title>
-</head>
+<?php require('./common/head.php') ?>
 
 <body>
-  <form method="POST" action="index.php">
-    <label for="Task">
-      Title:
-      <input type="text" name="title" id="Task" required>
-    </label>
-    <label for="Description">
-      Description:
-      <input type="text" name="description" id="Description" required>
-    </label>
-    <button type="submit">Send</button>
-  </form>
-
-  <a href="index.php">
-    Back
-  </a>
+  <main class="container">
+    <form class="form" method="POST" action="index.php?action=create">
+      <label class="label" for="Task">
+        Title:
+        <input class="input" type="text" name="title" id="Task" required>
+      </label>
+      <label class="label" for="Description">
+        Description:
+        <textarea class="input" name="description" id="Description" required></textarea>
+      </label>
+      <button class="btn btn-send" type="submit">Send</button>
+    </form>
+    <a class="btn btn-send" href="index.php">
+      Back
+    </a>
+  </main>
 </body>
 
 </html>
